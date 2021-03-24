a76a079e0445674c2c81816bc76879dcf7df0802
zzacong <ongzz100@gmail.com>
2021-03-25 00:10:52 +1100

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: `${__dirname}/uploads`,
-  filename: (req, file, callback) => {
+  filename: (_req, file, callback) => {
     const filename = `${Date.now()}${path.extname(file.originalname)}`
     callback(null, filename)
   },
@@ -17,7 +17,7 @@ const uploadImage = multer({ storage }).single('photo')
 app.use(cors())
 app.use(express.static('dist'))
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
@@ -27,7 +27,7 @@ app.post('/image', uploadImage, (req, res) => {
   res.send('Image upload failed')
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log('Server is listening on http://localhost:3000')
-})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () =>
+  console.log(`Server is listening on http://localhost:${PORT}`)
+)
